3fd99ef44dbee37655a0787380185369b4aca12a
Jelle <jelle@famvandenbrink.net>
2025-09-16 12:54:44 +0200

    add auto deploy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './pages/main/main.page';
 
-export const routes: Routes = [{ path: '**', component: MainPage }];
+export const routes: Routes = [
+  { path: '', component: MainPage },
+
+  { path: '**', redirectTo: '' },
+];
