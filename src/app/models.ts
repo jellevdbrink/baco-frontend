4576c14cb83401fbe75792caedb055af343d1a9a
Jelle <jelle@famvandenbrink.net>
2025-09-16 11:25:40 +0200

    add models

---
+++ src/app/models.ts
@@ -0,0 +1,37 @@
+export type Team = {
+  id: number;
+  name: string;
+  start_date: Date;
+}
+
+export type TeamMember = {
+  id: number;
+  name: string;
+  team: Team;
+}
+
+export type Category = {
+  id: number;
+  name: string;
+  icon: string;
+}
+
+export type Product = {
+  id: number;
+  name: string;
+  image: string;
+  description: string;
+  price: number;
+  category: Category;
+}
+
+export type OrderItem = {
+  product: Product;
+  quantity: number;
+}
+
+export type Order = {
+  datetime: Date;
+  by: number;
+  items: OrderItem[];
+}
